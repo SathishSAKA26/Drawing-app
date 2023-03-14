d312a94828fd2be62ad54f475cf8162a84a8859e
sathish <2000sathishkumar.d@gmail.com>
2023-03-14 20:10:23 +0530

    add some code

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// get elements
+const canvasEl = document.getElementById('canvas');
+const decBtn = document.getElementById('decrease');
+const incBtn = document.getElementById('increase');
+const 
